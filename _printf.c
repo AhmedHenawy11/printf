2e1e443aa144fbe1faf5de64ed2808dfc7063675
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-15 23:40:40 +0300

    ridiculousness cavity

--- _printf.c
+++ _printf.c
@@ -1,58 +1,58 @@
 #include "main.h"
 /**
- * _printf - writes the character c to stdout
+ * _printf - writes the character to stdout
  * @format: the specifier char that determine the type of data to be printed
- *
- * Return: 
+ * Description: we make our printf function AHMED @ ALAA
+ * Return: number of  characters printed excluding the null byte used to end output to strings.
  * On error, -1 is returned.
  */
 
 int _printf(const char *format, ...)
 {
-    va_list argument;
-    int n = 0, c = 0;
+	va_list argument;
+	int n = 0, c = 0;
 
-    if (format == NULL)
-    {
-        return (1);
-    }
-    va_start(argument, format);
-    while (*format)
-    {
-        if (*format != "\0")
-        {
-            _putchar(*format);
-            n++;
-        }
-        else
-        {
-            switch (*++format)
-            {
-                case '%':
-                    _putchar('%');
-                    n++;
-                    break;
-                case 'd':
-                    __putchar(va_arg(argument, int));
-                    n++;
-                    break;
-                case 'c':
-                    __putchar(va_arg(argument, int));
-                    n++;
-                    break;
-                case 's':
-                    char *str = va_arg(argument, char*);
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(argument, format);
+	while (*format)
+	{
+		if (*format != "\0")
+		{
+			_putchar(*format);
+			n++;
+		}
+		else
+		{
+			switch (*++format)
+			{
+				case '%':
+					_putchar('%');
+					n++;
+					break;
+				case 'd':
+					__putchar(va_arg(argument, int));
+					n++;
+					break;
+				case 'c':
+					__putchar(va_arg(argument, int));
+					n++;
+					break;
+				case 's':
+					char *str = va_arg(argument, char*);
 
-                    while (str)
-                    {
-                        c++;
-                    }
-                    write(1, str, c);
-                    n += c;
-                    break;
-            }
-        }
-    }format ++;
-    va_end(argument);
-    return (c);
+					while (str)
+					{
+						c++;
+					}
+					write(1, str, c);
+					n += c;
+					break;
+			}
+		}
+	}format ++;
+	va_end(argument);
+	return (c);
 }
